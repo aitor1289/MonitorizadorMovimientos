1d79597616209cae1b6d84736702306e17d1d0d7
aitor1289 <aitor1289@gmail.com>
2017-02-26 20:08:37 +0100

    Inicio construccion persistencia

---
+++ mobile/src/main/java/com/arriaga/aitor/monitorizadormovimientos/RegistroMovimientoEntity.java
@@ -0,0 +1,94 @@
+package com.arriaga.aitor.monitorizadormovimientos;
+
+/**
+ * Created by goati on 26/02/2017.
+ */
+
+public class RegistroMovimientoEntity {
+    private int ID_REG_MOV;
+    private int X;
+    private int Y;
+    private int Z;
+    private int TIPO_MOVIMIENTOS_ID_TIPO_MOV;
+    private int USUARIOS_ID_USER;
+    private int ID_TIPO_MOV_DETECT;
+
+    public RegistroMovimientoEntity(int ID_REG_MOV, int x, int y, int z, int TIPO_MOVIMIENTOS_ID_TIPO_MOV, int USUARIOS_ID_USER, int ID_TIPO_MOV_DETECT) {
+        this.ID_REG_MOV = ID_REG_MOV;
+        X = x;
+        Y = y;
+        Z = z;
+        this.TIPO_MOVIMIENTOS_ID_TIPO_MOV = TIPO_MOVIMIENTOS_ID_TIPO_MOV;
+        this.USUARIOS_ID_USER = USUARIOS_ID_USER;
+        this.ID_TIPO_MOV_DETECT = ID_TIPO_MOV_DETECT;
+    }
+
+    @Override
+    public String toString() {
+        return "RegistroMovimientoEntity{" +
+                "ID_REG_MOV=" + ID_REG_MOV +
+                ", X=" + X +
+                ", Y=" + Y +
+                ", Z=" + Z +
+                ", TIPO_MOVIMIENTOS_ID_TIPO_MOV=" + TIPO_MOVIMIENTOS_ID_TIPO_MOV +
+                ", USUARIOS_ID_USER=" + USUARIOS_ID_USER +
+                ", ID_TIPO_MOV_DETECT=" + ID_TIPO_MOV_DETECT +
+                '}';
+    }
+
+    public int getID_REG_MOV() {
+        return ID_REG_MOV;
+    }
+
+    public void setID_REG_MOV(int ID_REG_MOV) {
+        this.ID_REG_MOV = ID_REG_MOV;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public int getZ() {
+        return Z;
+    }
+
+    public void setZ(int z) {
+        Z = z;
+    }
+
+    public int getTIPO_MOVIMIENTOS_ID_TIPO_MOV() {
+        return TIPO_MOVIMIENTOS_ID_TIPO_MOV;
+    }
+
+    public void setTIPO_MOVIMIENTOS_ID_TIPO_MOV(int TIPO_MOVIMIENTOS_ID_TIPO_MOV) {
+        this.TIPO_MOVIMIENTOS_ID_TIPO_MOV = TIPO_MOVIMIENTOS_ID_TIPO_MOV;
+    }
+
+    public int getUSUARIOS_ID_USER() {
+        return USUARIOS_ID_USER;
+    }
+
+    public void setUSUARIOS_ID_USER(int USUARIOS_ID_USER) {
+        this.USUARIOS_ID_USER = USUARIOS_ID_USER;
+    }
+
+    public int getID_TIPO_MOV_DETECT() {
+        return ID_TIPO_MOV_DETECT;
+    }
+
+    public void setID_TIPO_MOV_DETECT(int ID_TIPO_MOV_DETECT) {
+        this.ID_TIPO_MOV_DETECT = ID_TIPO_MOV_DETECT;
+    }
+}
---
+++ mobile/src/main/java/com/arriaga/aitor/monitorizadormovimientos/TipoMovimientoBean.java
@@ -0,0 +1,51 @@
+package com.arriaga.aitor.monitorizadormovimientos;
+
+/**
+ * Created by goati on 26/02/2017.
+ */
+
+public class TipoMovimientoBean {
+    private int ID_TIPO_MOV;
+    private String TYPE;
+    private String DESCRIPTION;
+
+    public TipoMovimientoBean(int ID_TIPO_MOV, String TYPE, String DESCRIPTION) {
+        this.ID_TIPO_MOV = ID_TIPO_MOV;
+        this.TYPE = TYPE;
+        this.DESCRIPTION = DESCRIPTION;
+    }
+
+    @Override
+    public String toString() {
+        return "TipoMovimientoEntity{" +
+                "ID_TIPO_MOV=" + ID_TIPO_MOV +
+                ", TYPE='" + TYPE + '\'' +
+                ", DESCRIPTION='" + DESCRIPTION + '\'' +
+                '}';
+    }
+
+    public int getID_TIPO_MOV() {
+        RolEntity
+        return ID_TIPO_MOV;
+    }
+
+    public void setID_TIPO_MOV(int ID_TIPO_MOV) {
+        this.ID_TIPO_MOV = ID_TIPO_MOV;
+    }
+
+    public String getTYPE() {
+        return TYPE;
+    }
+
+    public void setTYPE(String TYPE) {
+        this.TYPE = TYPE;
+    }
+
+    public String getDESCRIPTION() {
+        return DESCRIPTION;
+    }
+
+    public void setDESCRIPTION(String DESCRIPTION) {
+        this.DESCRIPTION = DESCRIPTION;
+    }
+}
